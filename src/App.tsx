0b7d56e0d59d854a8bf1df903fd00108de4dfab5
kaypa <jose_kaypa@hotmail.com>
2023-11-10 19:06:02 -0500

    first profile

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Profile from './components/Profile'
+// import VideoBg from './components/VideoBg'
+
+function App() {
+  
+
+  return (
+    <div className=''>
+      {/* <VideoBg /> */}
+      <Profile />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import '../styles/profile.css'
+import photo from '../assets/Foto.jpg'
+
+export default function Profile() {
+  return (
+    <section className="profile-section">
+      <div></div>
+      <main>
+        <picture>
+          <img src={photo} alt="" className="photo" />
+        </picture>
+
+        <article>
+          <div className="profile-line-height">
+            <p style={{ textAlign: 'left' }}>Hello World !</p>
+            <div className="inline">
+              <span>I'm</span>
+              <span className="profile-name">
+                <span className="profile-strong"> J</span>ose{' '}
+                <strong className="profile-strong">K</strong>aypa
+              </span>
+            </div>
+          </div>
+          <p className="profile-stack">Fullstack Software Developer</p>
+          <p className="profile-description">Software Engineering student</p>
+        </article>
+      </main>
+    </section>
+  )
+}
