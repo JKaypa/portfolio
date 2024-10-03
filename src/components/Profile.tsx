787ac4b81747332d1c3f9790bf95d41b859cdab7
JKaypa <jose_kaypa@hotmail.com>
2024-10-03 13:41:32 -0500

    * refactor CSS classnames following BEM methodology structure

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,61 +1,70 @@
 import "../styles/profile.css";
 import photo from "../assets/Foto.jpg";
 import enResume from "../assets/FullstackJKaypa.pdf";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithubSquare, FaWhatsappSquare } from "react-icons/fa";
+import { LiaLinkedinIn, LiaGithub, LiaWhatsapp } from "react-icons/lia";
 import { IconContext } from "react-icons";
 
-export default function Profile() {
+export default function Profile(): JSX.Element {
   return (
-    <section className="profile-section">
-      <div></div>
+    <section className="profile">
+      <div className="profile__square"></div>
       <picture>
-        <img src={photo} alt="Jose Kaypa photography" className="photo" />
+        <img src={photo} alt="Jose Kaypa photography" className="profile__photo" />
       </picture>
 
-      <article>
-        <div className="profile-line-height">
-          <p style={{ textAlign: "left" }}>Hello World !</p>
-          <div className="inline">
-            <span>I'm</span>
+      <article className="profile__info">
+        <div className="profile__welcome">
+          <p>Hello World! 🌎</p>
+          <div className="profile__introduce">
+            <span className="profile__pronoun">I'm</span>
 
-            <span className="profile-name">Jose</span>
-            <span className="profile-name">Kaypa</span>
+            <span className="profile__name">Jose</span>
+            <span className="profile__name">Kaypa</span>
           </div>
         </div>
-        <p className="profile-stack">Fullstack Developer</p>
+        <p className="profile__role">Software Developer</p>
 
-        <p className="profile-description">
+        <p className="profile__description">
           I am a Full Stack web developer with formal studies in Software Engineering. In addition
           to having experience in both Front-end and Back-end technologies, I also possess strong
           knowledge of cloud computing and Linux server management. I strive to deliver quality
           products and ensure an appropriate software development life cycle.
         </p>
 
-        <div className="profile-icons">
-          <div className="profile-resume">
-            <a href={enResume} target="_blank" rel="noopener noreferrer" className="resume-english">
+        <div className="profile__buttons">
+          <div className="profile__resume">
+            <a
+              href={enResume}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="button button--link-resume"
+            >
               English
             </a>
-            <a href={enResume} target="_blank" rel="noopener noreferrer" className="resume-spanish">
+            <a
+              href={enResume}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="button button--link-resume"
+            >
               Spanish
             </a>
-            <button className="resume-button">My Resume</button>
+            <button className="button button--resume">My Resume</button>
           </div>
-          <IconContext.Provider value={{ className: "profile-social_icon" }}>
-            <div className="profile-social">
+          <IconContext.Provider value={{ className: "profile__social-icons" }}>
+            <div className="profile__icons">
               <a
                 href="https://www.linkedin.com/in/jkaypa/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaLinkedin />
+                <LiaLinkedinIn />
               </a>
               <a href="https://github.com/JKaypa  " target="_blank" rel="noopener noreferrer">
-                <FaGithubSquare />
+                <LiaGithub />
               </a>
               <a href="https://wa.me/573013111395" target="_blank" rel="noopener noreferrer">
-                <FaWhatsappSquare />
+                <LiaWhatsapp />
               </a>
             </div>
           </IconContext.Provider>
