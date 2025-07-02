51cf34a3d8239e4514636bcfd9061a55e9e02e45
JKaypa <jose_kaypa@hotmail.com>
2025-07-02 10:10:09 -0500

    create project data

---
+++ src/components/projects/projectData.ts
@@ -0,0 +1,23 @@
+type Project = {
+  name: string
+  src: string
+  alt: string
+  description: string
+}
+
+const projectData: Project[] = [
+  {
+    name: 'Travel-App',
+    src: 'src/assets/projects/travel-app-project.webp',
+    alt: 'travel app project image',
+    description: 'Book your next trip with your loved ones',
+  },
+  {
+    name: 'Travel Nerd',
+    src: 'src/assets/projects/travel-nerd-project.webp',
+    alt: 'travel nerd project image',
+    description: 'Document your most exiting trips and share them with anyone',
+  },
+]
+
+export { projectData }
