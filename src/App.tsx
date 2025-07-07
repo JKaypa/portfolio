7d3d468613aaac739ef94f770f860d72e67871be
JKaypa <jose_kaypa@hotmail.com>
2025-07-07 18:11:30 -0500

    rename files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Profile from './components/Profile'
-import { Projects } from './components/projects/Projects.tsx'
+import Profile from './components/profile/Profile.tsx'
+import { Projects } from './components/projects/ProjectsSection.tsx'
 
 function App() {
   return (
