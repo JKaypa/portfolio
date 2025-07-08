3e8901184031ebda3e56d4942f16e209d6227475
JKaypa <jose_kaypa@hotmail.com>
2025-07-08 17:43:12 -0500

    create modal

--- src/components/projects/ProjectsSection.tsx
+++ src/components/projects/ProjectsSection.tsx
@@ -1,24 +1,42 @@
 import { projectData } from './projectData.ts'
 import { Project } from './Project.tsx'
 import './project.css'
+import { useRef } from 'react'
 
 const Projects: React.FC = () => {
+  const dialogRef = useRef<Record<string, HTMLDialogElement | null>>({})
+
+  const handleShowModal = (modalId: number) => {
+    dialogRef.current[modalId]?.showModal()
+  }
+
+  const handleCloseModal = (modalId: number) => {
+    dialogRef.current[modalId]?.close()
+  }
+
   return (
     <>
       <header className="project-header">
         <h1 className="project-title">My Work</h1>
-        <p className="project-subtitle">These are some of my projects</p>
+        <p className="project-subtitle">
+          These are some of my projects, click them to see more details.
+        </p>
       </header>
 
       <div className="project-grid">
-        {projectData.map((project) => (
-          <Project
-            key={project.name}
-            imgSrc={project.src}
-            name={project.name}
-            alt={project.alt}
-            description={project.description}
-          />
+        {projectData.map(({ id, name, src, alt, description }) => (
+          <div key={id}>
+            <button onClick={() => handleShowModal(id)} className="project-button__modal">
+              <Project imgSrc={src} name={name} alt={alt} description={description} />
+            </button>
+
+            <dialog ref={(dialog) => (dialogRef.current[id] = dialog)}>
+              <h1>{name}</h1>
+              <img src={src} alt={alt} />
+              <p>{description}</p>
+              <button onClick={() => handleCloseModal(id)}>Close</button>
+            </dialog>
+          </div>
         ))}
       </div>
     </>
