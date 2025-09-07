416f6222f6e13dde5b98f766e19170df5062c09c
JKaypa <jose_kaypa@hotmail.com>
2025-09-06 20:01:09 -0500

    Add Modal component with openModal and closeModal functionality

---
+++ app/components/projects/components/modal/modal.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React, { useCallback, useEffect, useRef } from "react";
+import './modal.css'
+
+type CloseModal = {
+  onCloseModal: () => void
+}
+
+type Props = {
+  id: number,
+  children: React.ReactElement<CloseModal>
+}
+
+const Modal: React.FC<Props> = ({ id, children }) => {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+
+  const openModal = useCallback((event: CustomEvent<number>) => {
+    if(event.detail === id)
+      dialogRef.current?.showModal()
+  }, [id])
+
+  useEffect(() => {
+    document.body.addEventListener('open-dialog-modal', openModal as EventListener)
+
+    return () =>
+      document.body.removeEventListener('open-dialog-modal', openModal as EventListener)
+
+  }, [openModal])
+  
+
+  const handleCloseModal = () => {
+      dialogRef.current?.close()
+  }
+
+
+  return(
+      // @ts-expect-error closedBy is a valid prop
+  <dialog ref={dialogRef} closedby="any" className="modal">
+    { React.cloneElement<CloseModal>(children, { onCloseModal: handleCloseModal }) }
+  </dialog>)
+}
+
+export default Modal
