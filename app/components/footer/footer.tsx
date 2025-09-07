b245aa2930bba0911e5ddd5d1b8cdf023292f6ff
JKaypa <jose_kaypa@hotmail.com>
2025-09-06 19:57:02 -0500

    Refactor Footer component: replace inline year logic with FullYear component and update copyright text

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,13 +1,14 @@
 import './footer.css'
+import FullYear from "@/app/components/footer/components/full-year";
 
 const Footer: React.FC = () => {
   return (
       <>
         <div className="footer"></div>
         <footer className="footer__content">
-          <span>{new Date().getFullYear()}</span>
+          <FullYear />
           <span>Jose Kaypa</span>
-          <span></span>
+          <span><span className="footer__rights-logo">©</span> All rights reserved</span>
         </footer>
       </>
   )
