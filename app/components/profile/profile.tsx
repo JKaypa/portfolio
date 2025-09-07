59cc951a088ae48d57cd804a3cfc2899bcc79b68
JKaypa <jose_kaypa@hotmail.com>
2025-09-06 19:56:00 -0500

    Refactor Profile component to use ContactIcons component for social links

--- app/components/profile/profile.tsx
+++ app/components/profile/profile.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import './profile.css'
+import ContactIcons from "@/app/components/contact/contact-icons";
 
 const Profile: React.FC = () => {
   return (
@@ -34,39 +35,7 @@ const Profile: React.FC = () => {
             <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="button">
               My Resume
             </a>
-
-            <div className="profile__icons">
-              <a
-                  href="https://www.linkedin.com/in/jkaypa"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="See my Likedin profile"
-                  className="profile__social-icons"
-              >
-                <Image src="/icons/linkedin.svg" alt="linkedin logo" width={30} height={30}/>
-              </a>
-
-              <a
-                  href="https://github.com/JKaypa"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="See my GitHub profile"
-                  className="profile__social-icons"
-              >
-                <Image src="/icons/github.svg" alt="github logo" width={30} height={30}/>
-              </a>
-
-              <a
-                  href="https://wa.me/573013111395"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Contact me on whatsapp"
-                  className="profile__social-icons"
-              >
-                <Image src="/icons/whatsapp.svg" alt="whatsapp logo" width={30} height={30}/>
-              </a>
-            </div>
-
+            <ContactIcons />
           </div>
         </article>
       </section>
