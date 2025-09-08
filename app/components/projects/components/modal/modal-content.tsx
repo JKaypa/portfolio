2839396da7fec1aa656b0c5440923d3dddf75220
JKaypa <jose_kaypa@hotmail.com>
2025-09-08 14:13:11 -0500

    Add ModalContent component with project details, technologies, and links

---
+++ app/components/projects/components/modal/modal-content.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import './modal.css'
+
+type Props = {
+  name: string,
+  src: string,
+  alt: string,
+  description: string,
+  techs: { src: string; name: string }[],
+  links: {
+    github: string
+    live?: string
+  }
+}
+
+const ModalContent: React.FC<Props> = ({ src, alt, techs, links, description, name }) => {
+
+  return (
+    <div className="modal-content">
+      <section className="modal-content__head">
+        <div className="modal-content__title">
+          <h1>{name}</h1>
+        </div>
+        <Image src={src} alt={alt} width={450} height={255}/>
+        <p className="modal-content__description">{description}</p>
+      </section>
+
+      <section>
+        <h2>Technologies</h2>
+        <div className="modal-content__project-techs">
+          {techs.map(({src, name}) => (
+              <div key={src}>
+                <Image src={src} alt={name} width={30} height={30} className="modal-content__tech__icons"/>
+                <p>{name}</p>
+              </div>
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <h2>Give it a chek</h2>
+        <div className="modal-content__tech__links">
+          <a href={links.github} target="_blank" rel="noopener noreferrer">
+            <Image src="/icons/github.svg" alt="github" width={30} height={30}
+                   className="modal-content__tech__icons"/>
+            <p>Source code</p>
+          </a>
+          {links.live && (
+              <a href={links.live} target="_blank" rel="noopener noreferrer">
+                <Image src="/icons/live.svg" alt="live demo" width={30} height={30}
+                       className="modal-content__tech__icons"/>
+                <p>Live demo</p>
+              </a>
+          )}
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default ModalContent
