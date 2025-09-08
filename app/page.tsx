962b4f5c559a6c14ddfbe50fe70841de72312acf
JKaypa <jose_kaypa@hotmail.com>
2025-09-07 22:43:57 -0500

    Add Footer component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Profile from "@/app/components/profile/profile";
 import ProjectList from "@/app/components/projects/project-list";
+import Footer from "@/app/components/footer/footer";
 
 export default function Home() {
   return (
-    <><Profile/><ProjectList /></>
+    <><Profile/><ProjectList /><Footer /></>
   );
 }
