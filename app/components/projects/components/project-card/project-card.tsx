e8b6809634cc0b4b89776ec7522289b52e4716ec
JKaypa <jose_kaypa@hotmail.com>
2025-09-09 21:46:02 -0500

    Refactor ProjectCard to use unified Project type for prop structure simplification

--- app/components/projects/components/project-card/project-card.tsx
+++ app/components/projects/components/project-card/project-card.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import './project-card.css'
+import Project from "@/app/components/projects/libs/types";
 
 type Props = {
-  alt: string
-  imgSrc: string
-  name: string
-  description: string
+  project: Project
 }
 
-const ProjectCard: React.FC<Props> = ({ imgSrc, alt, name, description }) => {
+const ProjectCard: React.FC<Props> = ({ project }) => {
+  const { name, src, alt, description } = project
+
   return (
       <div className="project-card">
-        <Image src={imgSrc} alt={alt} width={450} height={255} className="project-card__image" />
+        <Image src={src} alt={alt} width={450} height={255} className="project-card__image" />
         <h2 className="project-card__title">{name}</h2>
         <p className="project-card__description">{description}</p>
       </div>
