6a2f27f75f0814acef693956f7cde07455fe30c9
JKaypa <jose_kaypa@hotmail.com>
2025-09-11 21:56:20 -0500

    Add ContactForm component to Home page for user inquiries

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Profile from "@/app/components/profile/profile";
 import Projects from "@/app/components/projects/projects";
 import Footer from "@/app/components/footer/footer";
+import ContactForm from "@/app/components/contact/contact-form/contact-form";
 
 export default function Home() {
   return (
-    <><Profile/><Projects /><Footer /></>
+      <>
+        <Profile/>
+        <Projects/>
+        <ContactForm/>
+        <Footer/>
+      </>
   );
 }
