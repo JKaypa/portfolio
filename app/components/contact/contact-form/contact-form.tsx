37e3003993d6f5f5c01146d0b59bb45a189b64a2
JKaypa <jose_kaypa@hotmail.com>
2025-10-15 16:57:33 -0500

    Refactor ContactForm component: replace `Image` and `Contact` components with a new form implementation using `next/form` and add submission handling logic.

--- app/components/contact/contact-form/contact-form.tsx
+++ app/components/contact/contact-form/contact-form.tsx
@@ -1,39 +1,59 @@
-import Image from 'next/image'
-import Contact from "@/app/components/contact/contact-form/contact";
-import './contact-form.css'
+import Form from "next/form";
+
 
 const ContactForm: React.FC = () => {
 
-  return (
-    <section className="section-bottom-padding">
-      <header className="section-header">
-        <h1 className="section-title">
-          Let&#39;s Work Together
-        </h1>
-        <p className="section-subtitle">
-          Got a project that needs expert development?
-          You&#39;ve found one.
 
-        </p>
-      </header>
-      <div className="contact-form-container">
-        <div className="contact-form-image">
-          <Image
-            src="/images/programming.webp"
-            alt="Programming workspace"
-            width={400}
-            height={500}
-            className="contact-image"
-          />
-        </div>
-        
-        <div className="contact-form-content">
-          <h1 className="contact-form-title">Contact me</h1>
-          <Contact />
-        </div>
-      </div>
-    </section>
-  )
+  const handleSubmit = async (formData: FormData) => {
+    'use server'
+
+  }
+
+  return <Form action={handleSubmit} className="contact-form">
+
+    <label className="form-group">
+      Name
+      <input
+          type="text"
+          name="name"
+          className="form-input"
+          placeholder="Your name"
+          minLength={3}
+          required
+      />
+    </label>
+
+
+    <label htmlFor="email" className="form-group">
+      Email
+      <input
+          type="email"
+          id="email"
+          name="email"
+          className="form-input"
+          placeholder="Your email"
+          required
+      />
+    </label>
+
+
+    <label htmlFor="message" className="form-group">
+      Message
+      <textarea
+          id="message"
+          name="message"
+          className="form-textarea"
+          placeholder="Tell me about your project..."
+          rows={5}
+          minLength={10}
+          required
+      />
+    </label>
+
+    <button type="submit" className="form-submit-btn">
+      Send Message
+    </button>
+  </Form>
 }
 
 export default ContactForm
