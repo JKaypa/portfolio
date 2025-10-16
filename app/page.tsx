7ff76fa65b0b291bd2bd4be17b79093411350b5e
JKaypa <jose_kaypa@hotmail.com>
2025-10-16 11:43:01 -0500

    Rename `ContactForm` to `ContactMe` in `Home` component for clarity and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Profile from "@/app/components/profile/profile";
 import Projects from "@/app/components/projects/projects";
 import Footer from "@/app/components/footer/footer";
-import ContactForm from "@/app/components/contact/contact-form/contact-form";
+import ContactMe from "@/app/components/contact/contact-form/contact-me";
 
 export default function Home() {
   return (
       <>
         <Profile/>
         <Projects/>
-        <ContactForm/>
+        <ContactMe/>
         <Footer/>
       </>
   );
