51591a8623f274739080eaba66b6741f2271de18
JKaypa <jose_kaypa@hotmail.com>
2025-11-01 09:34:27 -0500

    Add email API route with rate limiting, validation, and send functionality

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,38 @@
+import rateLimit from "@/app/api/send-email/rate-limiter";
+import validate from "@/app/api/validation";
+import {NextRequest} from "next/server";
+import sendEmail from "@/app/api/send-email/send-email";
+
+
+
+export async function POST(req: NextRequest): Promise<Response>  {
+  const ip = req.headers.get('x-forwarded-for') || req.headers.get('x-real-ip')
+
+  const isLimitReached = await rateLimit(ip)
+
+  if(isLimitReached){
+    return Response.json({ error: "Too many requests, please try again later" })
+  }
+
+  const userData = await req.formData()
+
+  const [isValid, user] = validate(userData)
+
+  if(!isValid){
+    return Response.json({ error: "Invalid data" })
+  }
+
+
+  try{
+    const error = await sendEmail(user)
+
+    if(error){
+      return Response.json({ error: error })
+    }
+
+    return Response.json({ message: 'Email sent successfully' })
+  }catch (error){
+    return Response.json({ unknownError: error })
+  }
+
+}
