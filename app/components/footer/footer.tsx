02c8f93b5f72d8cf5dc4dfa11ad89f48ffe35069
JKaypa <jose_kaypa@hotmail.com>
2025-11-13 16:54:15 -0500

    Refactor `Footer` component: clean up formatting, update CSS for centered layout and spacing

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,19 +1,17 @@
-import FullYear from "@/app/components/footer/components/full-year";
+import FullYear from '@/app/components/footer/components/full-year'
 import './footer.css'
 
 const Footer: React.FC = () => {
   return (
-      <>
-        <div className="footer"></div>
-        <footer className="footer__content">
-          <span>
-            <span className="footer__rights-logo">©</span>
-            <FullYear/>
-            <span> Jose Kaypa</span>
-            <span> All rights reserved</span>
-          </span>
-        </footer>
-      </>
+    <>
+      <div className="footer"></div>
+      <footer className="footer__content">
+        <span className="footer__rights-logo">©</span>
+        <FullYear />
+        <span> Jose Kaypa</span>
+        <span> All rights reserved</span>
+      </footer>
+    </>
   )
 }
 
