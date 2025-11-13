99fe1db3c0e2b06bd49c5593cf78e9a6dd2480e6
JKaypa <jose_kaypa@hotmail.com>
2025-11-13 16:11:15 -0500

    Refactor `ContactForm` component: add server response handling, improve validation, and clean up structure

--- app/components/contact/contact-form/contact-form.tsx
+++ app/components/contact/contact-form/contact-form.tsx
@@ -1,45 +1,39 @@
-import Form from "next/form";
+'use client'
 
+import Form from 'next/form'
+import { toast } from 'sonner'
 
 const ContactForm: React.FC = () => {
-
-
   const handleSubmit = async (formData: FormData) => {
-    'use server'
+    const res = await fetch('http://localhost:3000/api/send-email', {
+      method: 'POST',
+      body: formData,
+    })
 
+    if (!res.ok) {
+      const error = (await res.json()) as { message: string }
+      toast.error(error.message)
+    }
+
+    const success = (await res.json()) as { message: string }
+    toast.success(success.message)
   }
 
-  return <Form action={handleSubmit} className="contact-form">
+  return (
+    <Form action={handleSubmit} className="contact-form">
+      <label className="form-group">
+        Subject
+        <input type="text" name="subject" className="form-input" placeholder="Add a subject" minLength={3} required />
+      </label>
 
-    <label className="form-group">
-      Name
-      <input
-          type="text"
-          name="name"
-          className="form-input"
-          placeholder="Your name"
-          minLength={3}
-          required
-      />
-    </label>
+      <label htmlFor="email" className="form-group">
+        Email
+        <input type="email" id="email" name="email" className="form-input" placeholder="Your email" pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$" required />
+      </label>
 
-
-    <label htmlFor="email" className="form-group">
-      Email
-      <input
-          type="email"
-          id="email"
-          name="email"
-          className="form-input"
-          placeholder="Your email"
-          required
-      />
-    </label>
-
-
-    <label htmlFor="message" className="form-group">
-      Message
-      <textarea
+      <label htmlFor="message" className="form-group">
+        Message
+        <textarea
           id="message"
           name="message"
           className="form-textarea"
@@ -47,13 +41,14 @@ const ContactForm: React.FC = () => {
           rows={5}
           minLength={10}
           required
-      />
-    </label>
+        />
+      </label>
 
-    <button type="submit" className="form-submit-btn">
-      Send Message
-    </button>
-  </Form>
+      <button type="submit" className="form-submit-btn">
+        Send Message
+      </button>
+    </Form>
+  )
 }
 
 export default ContactForm
