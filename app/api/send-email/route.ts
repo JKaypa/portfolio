6108fe4994414564ed435a046de17f3ccb901eb9
JKaypa <jose_kaypa@hotmail.com>
2025-11-13 16:15:45 -0500

    Refactor `send-email` route: clean up formatting, adjust imports, and improve code consistency

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,38 +1,34 @@
-import rateLimit from "@/app/api/send-email/rate-limiter";
-import validate from "@/app/api/validation";
-import {NextRequest} from "next/server";
-import sendEmail from "@/app/api/send-email/send-email";
+import rateLimit from '@/app/api/send-email/rate-limiter'
+import validate from '@/app/api/validation'
+import { NextRequest } from 'next/server'
+import sendEmail from '@/app/api/send-email/send-email'
 
-
-
-export async function POST(req: NextRequest): Promise<Response>  {
+export async function POST(req: NextRequest): Promise<Response> {
   const ip = req.headers.get('x-forwarded-for') || req.headers.get('x-real-ip')
 
   const isLimitReached = await rateLimit(ip)
 
-  if(isLimitReached){
-    return Response.json({ error: "Too many requests, please try again later" })
+  if (isLimitReached) {
+    return Response.json({ error: 'Too many requests, please try again later' })
   }
 
   const userData = await req.formData()
 
   const [isValid, user] = validate(userData)
 
-  if(!isValid){
-    return Response.json({ error: "Invalid data" })
+  if (!isValid) {
+    return Response.json({ error: 'Invalid data' })
   }
 
-
-  try{
+  try {
     const error = await sendEmail(user)
 
-    if(error){
-      return Response.json({ error: error })
+    if (error) {
+      return Response.json({ message: error.message })
     }
 
     return Response.json({ message: 'Email sent successfully' })
-  }catch (error){
+  } catch (error) {
     return Response.json({ unknownError: error })
   }
-
 }
