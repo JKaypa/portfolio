84f7a559310b1a194354a23a5f4db49ea51048bf
JKaypa <jose_kaypa@hotmail.com>
2025-11-13 16:11:48 -0500

    Add `Toaster` component to `RootLayout` for toast notifications and adjust code formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,42 @@
-import type { Metadata } from "next";
-import { Oswald, Open_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Oswald, Open_Sans } from 'next/font/google'
+import { Toaster } from 'sonner'
+import './globals.css'
 
 const oswald = Oswald({
-  subsets: ["latin"],
-  weight: "500",
+  subsets: ['latin'],
+  weight: '500',
 })
 
 const openSans = Open_Sans({
-  subsets: ["latin"],
-  weight: "500",
-  variable: "--open-sans"
+  subsets: ['latin'],
+  weight: '500',
+  variable: '--open-sans',
 })
 
 export const metadata: Metadata = {
-  title: "Jose Kaypa",
+  title: 'Jose Kaypa',
   description: "Jose Kaypa's portfolio",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={`${oswald.className} ${openSans.variable}`}>
         {children}
+        <Toaster
+          toastOptions={{
+            style: {
+              background: '#222',
+              color: '#fff',
+            },
+          }}
+        />
       </body>
     </html>
-  );
+  )
 }
