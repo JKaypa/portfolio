b55643a7a409225d3e2279c1a02d27087b9fc961
JKaypa <jose_kaypa@hotmail.com>
2025-11-17 10:07:52 -0500

    Add Turnstile validation to email endpoint for enhanced security and spam prevention

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -2,33 +2,39 @@ import rateLimit from '@/app/api/send-email/rate-limiter'
 import validate from '@/app/api/validation'
 import { NextRequest } from 'next/server'
 import sendEmail from '@/app/api/send-email/send-email'
+import validateTurnstile from '@/app/api/send-email/turnstile-validation'
 
 export async function POST(req: NextRequest): Promise<Response> {
   const ip = req.headers.get('x-forwarded-for') || req.headers.get('x-real-ip')
+  const emailData = await req.formData()
+  const token = emailData.get('cf-turnstile-response')
 
-  const isLimitReached = await rateLimit(ip)
+  const validation = await validateTurnstile(token, ip)
 
-  if (isLimitReached) {
-    return Response.json({ error: 'Too many requests, please try again later' })
+  if (!validation.success) {
+    return Response.json('Invalid verification', { status: 400 })
   }
 
-  const emailData = await req.formData()
+  const isLimitReached = await rateLimit(ip)
+  if (isLimitReached) {
+    return Response.json({ error: 'Too many requests, please try again later' }, { status: 429 })
+  }
 
   const [isValid, email] = validate(emailData)
 
   if (!isValid) {
-    return Response.json({ error: 'Invalid data' })
+    return Response.json({ error: 'Invalid data' }, { status: 400 })
   }
 
   try {
     const error = await sendEmail(email)
 
     if (error) {
-      return Response.json({ message: error.message })
+      return Response.json({ message: error.message }, { status: 500 })
     }
 
     return Response.json({ message: 'Email sent successfully' })
   } catch (error) {
-    return Response.json({ unknownError: error })
+    return Response.json({ unknownError: error }, { status: 500 })
   }
 }
