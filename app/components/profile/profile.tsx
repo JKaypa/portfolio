d86903c4d4b91ee85ecf45ea35480be73e4157d0
JKaypa <jose_kaypa@hotmail.com>
2025-11-20 17:52:24 -0500

    Refactor `profile` component: improve class structure, add `show` utility classes, and clean up JSX formatting.

--- app/components/profile/profile.tsx
+++ app/components/profile/profile.tsx
@@ -1,44 +1,49 @@
-import Image from "next/image";
+import Image from 'next/image'
 import './profile.css'
-import ContactIcons from "@/app/components/contact/contact-icons/contact-icons";
+import ContactIcons from '@/app/components/contact/contact-icons/contact-icons'
 
 const Profile: React.FC = () => {
   return (
-      <section className="profile">
-        <div className="profile__square"></div>
-        <picture>
-          <Image src="/images/photo.webp" alt="Jose Kaypa photography" width={350} height={406}
-                 fetchPriority="high" priority={true} className="profile__photo"
-          />
-        </picture>
+    <section className="profile">
+      <div className="profile__square show_square"></div>
+      <picture>
+        <Image
+          src="/images/photo.webp"
+          alt="Jose Kaypa photography"
+          width={350}
+          height={406}
+          fetchPriority="high"
+          priority={true}
+          className="profile__photo show_image"
+        />
+      </picture>
 
-        <article className="profile__info">
-          <div className="profile__welcome">
-            <p>Hello World! 🌎</p>
-            <div className="profile__introduce">
-              <span className="profile__pronoun">I&apos;m</span>
+      <article className="profile__info">
+        <div className="profile__welcome show_welcome">
+          <p>Hello World! 🌎</p>
+          <div className="profile__introduce">
+            <span className="profile__pronoun">I&apos;m</span>
 
-              <span className="profile__name">Jose</span>
-              <span className="profile__name">Kaypa</span>
-            </div>
+            <span className="profile__name">Jose</span>
+            <span className="profile__name">Kaypa</span>
           </div>
-          <p className="profile__role">Software Developer</p>
+        </div>
+        <p className="profile__role show_role">Software Developer</p>
 
-          <p className="profile__description">
-            I am a Full Stack developer with backend focus and basic knowledge of design principles.
-            Constantly looking for learning new concepts and better problem solving approaches that
-            let me build clean, scalable, mantenible and loosely coupled architectures that allow me
-            delivering quality products and ensure an appropriate software development life cycle.
-          </p>
+        <p className="profile__description show">
+          I am a Full Stack developer with backend focus and basic knowledge of design principles. Constantly looking for learning new concepts and
+          better problem solving approaches that let me build clean, scalable, mantenible and loosely coupled architectures that allow me delivering
+          quality products and ensure an appropriate software development life cycle.
+        </p>
 
-          <div className="profile__buttons">
-            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="button">
-              My Resume
-            </a>
-            <ContactIcons />
-          </div>
-        </article>
-      </section>
+        <div className="profile__buttons show">
+          <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="button">
+            My Resume
+          </a>
+          <ContactIcons />
+        </div>
+      </article>
+    </section>
   )
 }
 
