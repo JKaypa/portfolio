58b00818df7424754e575550229ed5a67bf2973c
JKaypa <jose_kaypa@hotmail.com>
2025-11-20 17:53:08 -0500

    Refactor `project-card` component: update class structure, clean up formatting, and introduce `auto-show-section` class for animations.

--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -1,30 +1,24 @@
-import ProjectList from "@/app/components/projects/components/project-list/project-list";
-import ProjectCard from "@/app/components/projects/components/project-card/project-card";
-import ModalContent from "@/app/components/projects/components/modal/modal-content";
-import projectData from "@/app/components/projects/project-data-list";
+import ProjectList from '@/app/components/projects/components/project-list/project-list'
+import ProjectCard from '@/app/components/projects/components/project-card/project-card'
+import ModalContent from '@/app/components/projects/components/modal/modal-content'
+import projectData from '@/app/components/projects/project-data-list'
 import './projects.css'
 
 const Projects: React.FC = () => {
-
   return (
-      <section className="section-bottom-padding">
-        <header className="section-header">
-          <h1 className="section-title">Check My Work</h1>
-          <p className="section-subtitle">
-            These are some of my projects and technologies I use. Click them to see more details.
-          </p>
-        </header>
-        <div className="project-grid">
-          {projectData.map((project) =>
-              <ProjectList
-                  key={ project.id }
-                  id={project.id}
-                  trigger={<ProjectCard project={project} />}
-              >
-                <ModalContent project={project}/>
-              </ProjectList>)}
-        </div>
-      </section>
+    <section className="section-bottom-padding">
+      <header className="section-header auto-show">
+        <h1 className="section-title">Check My Work</h1>
+        <p className="section-subtitle">These are some of my projects and technologies I use. Click them to see more details.</p>
+      </header>
+      <div className="project-grid">
+        {projectData.map((project) => (
+          <ProjectList key={project.id} id={project.id} trigger={<ProjectCard project={project} />}>
+            <ModalContent project={project} />
+          </ProjectList>
+        ))}
+      </div>
+    </section>
   )
 }
 
