89aab6d4cc4a0770ccb808add5025ddb8c30ca8e
JKaypa <jose_kaypa@hotmail.com>
2025-11-20 17:50:50 -0500

    Refactor `contact-me` component: clean up formatting, add utility classes, and simplify JSX structure.

--- app/components/contact/contact-form/contact-me.tsx
+++ app/components/contact/contact-form/contact-me.tsx
@@ -1,32 +1,19 @@
 import Image from 'next/image'
-import ContactForm from "@/app/components/contact/contact-form/contact-form";
+import ContactForm from '@/app/components/contact/contact-form/contact-form'
 import './contact-form.css'
 
 const ContactMe: React.FC = () => {
-
   return (
     <section className="section-bottom-padding">
-      <header className="section-header">
-        <h1 className="section-title">
-          Let&#39;s Work Together
-        </h1>
-        <p className="section-subtitle">
-          Got a project that needs expert development?
-          You&#39;ve found one.
-
-        </p>
+      <header className="section-header auto-show">
+        <h1 className="section-title">Let&#39;s Work Together</h1>
+        <p className="section-subtitle">Got a project that needs expert development? You&#39;ve found one.</p>
       </header>
-      <div className="contact-form-container">
+      <div className="contact-form-container auto-show-section">
         <div className="contact-form-image">
-          <Image
-            src="/images/programming.webp"
-            alt="Programming workspace"
-            width={400}
-            height={500}
-            className="contact-image"
-          />
+          <Image src="/images/programming.webp" alt="Programming workspace" width={400} height={500} className="contact-image" />
         </div>
-        
+
         <div className="contact-form-content">
           <h1 className="contact-form-title">Contact me</h1>
           <ContactForm />
