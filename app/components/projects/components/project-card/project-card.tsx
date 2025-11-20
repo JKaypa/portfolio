244c553ccfbddb07d104307282faa3be7274e57e
JKaypa <jose_kaypa@hotmail.com>
2025-11-20 17:53:01 -0500

    Refactor `project-card` component: update class structure, clean up formatting, and introduce `auto-show-section` class for animations.

--- app/components/projects/components/project-card/project-card.tsx
+++ app/components/projects/components/project-card/project-card.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import Image from 'next/image'
 import './project-card.css'
-import Project from "@/app/components/projects/libs/types";
+import Project from '@/app/components/projects/libs/types'
 
 type Props = {
   project: Project
@@ -10,11 +10,11 @@ const ProjectCard: React.FC<Props> = ({ project }) => {
   const { name, src, alt, description } = project
 
   return (
-      <div className="project-card">
-        <Image src={src} alt={alt} width={450} height={255} className="project-card__image" />
-        <h2 className="project-card__title">{name}</h2>
-        <p className="project-card__description">{description}</p>
-      </div>
+    <div className="project-card auto-show-section">
+      <Image src={src} alt={alt} width={450} height={255} className="project-card__image" />
+      <h2 className="project-card__title">{name}</h2>
+      <p className="project-card__description">{description}</p>
+    </div>
   )
 }
 
